e4278c1454f37cfb14134515101677ae72441668
ridhwandaud <m.ridhwan.daud@gmail.com>
2017-04-29 17:15:22 +0800

    add shadow using elavation for android

--- src/components/header.js
+++ src/components/header.js
@@ -14,18 +14,15 @@ const Header = () => {
 };
 
 const styles = {
-	viewStyle:{
+	viewStyle: {
 		backgroundColor: '#f8f8f8',
-		justifyContent : 'center',
-		alignItems : 'center',
-		height : 60,
-		shadowColor: '#000',
-     	shadowOffset: { width: 0, height: 2 },
-     	shadowOpacity: 0.2,
-     	elevation: 2,
-     	position: 'relative'
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60,
+		elevation: 5,
+		position: 'relative'
 	},
-	textStyle:{
+	textStyle: {
 		fontSize: 20
 	}
 };
