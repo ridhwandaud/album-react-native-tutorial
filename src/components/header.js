2411cf79c586aae890d6a65e1ddb29db4a4a2ff8
ridhwandaud <m.ridhwan.daud@gmail.com>
2017-04-29 16:15:26 +0800

    add styling to header

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,15 @@ import { Text } from 'react-native';
 
 // Make a component
 const Header = () => {
-	return <Text>Albums!</Text>;
+	const { textStyle } = styles;
+
+	return <Text style = {textStyle} >Albums!</Text>;
+};
+
+const styles = {
+	textStyle:{
+		fontSize: 40
+	}
 };
 
 // Make the component available to ohter parts of the app
