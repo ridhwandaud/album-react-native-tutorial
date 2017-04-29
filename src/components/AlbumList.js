0992cde6312653638033a192b8be93874712a145
ridhwandaud <m.ridhwan.daud@gmail.com>
2017-04-29 21:02:32 +0800

    use class based components

---
+++ src/components/AlbumList.js
@@ -0,0 +1,15 @@
+// Import libraries for making a component
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class AlbumList extends Component {
+	render() {
+		return (
+			<View>
+				<Text>Album List!!</Text>	
+			</View>
+		);
+	}
+}
+
+export default AlbumList;
