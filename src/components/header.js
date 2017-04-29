a0f3fbaf30bf1e1eca0abb834e99da70625791f4
ridhwandaud <m.ridhwan.daud@gmail.com>
2017-04-29 17:28:40 +0800

    add dynamic header using props

--- src/components/header.js
+++ src/components/header.js
@@ -3,12 +3,12 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 // Make a component
-const Header = () => {
-	const { textStyle,viewStyle } = styles;
+const Header = (props) => {
+	const { textStyle, viewStyle } = styles;
 
 	return (
-		<View style = {viewStyle}>
-			<Text style = {textStyle} >Albums!</Text>
+		<View style={viewStyle}>
+			<Text style={textStyle} >{props.headerText}!</Text>
 		</View>
 	);
 };
