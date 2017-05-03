bfd80eba3a496f2fa79f910395f787dd2aad1cca
ridhwandaud <m.ridhwan.daud@gmail.com>
2017-05-03 21:41:43 +0800

    add axios for http request, use cwm to fetch data

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,8 +1,14 @@
 // Import libraries for making a component
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+	componentWillMount() {
+		console.log('componentWillMount in AlbumList');
+		axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+		.then(response => console.log(response));
+	}
 	render() {
 		return (
 			<View>
