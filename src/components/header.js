8e42d454048776e90d1e68eb1f20b2ec85eb93fd
ridhwan <muhammadridhwandaud@gmail.com>
2017-05-04 12:01:54 +0800

    render data using many single module

--- src/components/header.js
+++ src/components/header.js
@@ -8,19 +8,23 @@ const Header = (props) => {
 
 	return (
 		<View style={viewStyle}>
-			<Text style={textStyle} >{props.headerText}!</Text>
+			<Text style={textStyle} >{props.headerText}</Text>
 		</View>
 	);
 };
 
 const styles = {
 	viewStyle: {
-		backgroundColor: '#f8f8f8',
-		justifyContent: 'center',
-		alignItems: 'center',
-		height: 60,
-		elevation: 5,
-		position: 'relative'
+		backgroundColor: '#F8F8F8',
+	    justifyContent: 'center',
+	    alignItems: 'center',
+	    height: 60,
+	    paddingTop: 15,
+	    shadowColor: '#000',
+	    shadowOffset: { width: 0, height: 2 },
+	    shadowOpacity: 0.2,
+	    elevation: 2,
+	    position: 'relative'
 	},
 	textStyle: {
 		fontSize: 20
